2f7e033fbc3bf06bc2897d9ba9de38b43212220d
khanhzodoi <khanhzodoi@gmail.com>
2018-08-13 17:31:37 +0700

    ex3_4_5

---
+++ ex3/my.py
@@ -0,0 +1,5 @@
+
+
+#Save 10.000đ everyday. What will my saving be in three months
+print("Saving 10.000 VND everyday. Then, I'll have", 30 * 3 * 10000,"\
+in 3 months(each month has 30 days by my settings)")
---
+++ ex5/ex5.py
@@ -0,0 +1,19 @@
+name = 'Pham Khanh'
+age = 21 # not a lie
+height_in_cm = 176 #centimeters
+height_in_inches = height_in_cm / 2.54 # inches
+weight_in_kg = 76 #kilograms
+weight_in_ibs = weight_in_kg / 0.453 #ibs
+eyes = 'Black'
+teeth = 'White'
+hair = 'Black'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height_in_cm} cm or {height_in_inches} inches tall.")
+print(f"He's {weight_in_kg} kg or {weight_in_ibs} pound heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+
+total = age + weight_in_kg + height_in_cm
+print(f"If I add {age}, {height_in_cm}, and {weight_in_kg} I get {total}.")
