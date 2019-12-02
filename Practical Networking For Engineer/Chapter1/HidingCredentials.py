9d395bcf9c38a1960ce06cbab66516e9a37c2e8c
khanhzodoi <khanhzodoi@gmail.com>
2019-12-02 15:16:28 +0700

    Hiding_Credentials

---
+++ Practical Networking For Engineer/Chapter1/HidingCredentials.py
@@ -0,0 +1,29 @@
+import getpass
+import base64
+
+#ask for username .. will be displayed when typed
+uname = input("Enter your username: ")
+
+#ask for password ... wont be displated when typed
+p = getpass.getpass(prompt="Enter your password: ")
+
+#construct credential with *.* as seperator between uname and pass
+creds=uname + '*.*' +p
+
+###Encrypt  a given set of credentials
+def encryptcredential(pwd):
+    rvalue=base64.b64encode(pwd.encode())
+    rvalue=rvalue.decode()
+    return rvalue
+
+###Decrypt a given set of credentials
+def decryptcredential(pwd):
+    rvalue=base64.b64decode(pwd)
+    rvalue=rvalue.decode()
+    return rvalue
+
+
+encryptedcreds=encryptcredential(creds)
+print ("Simple creds: " + creds)
+print ("Encrypted creds: " + encryptedcreds)
+print ("Decrypted creds: " + decryptcredential(encryptedcreds))
