4fa5bbb78a78e02791facbee87dbe48d0b119d4a
khanhzodoi <khanhzodoi@gmail.com>
2019-12-27 22:17:23 +0700

    Practice Usecase 1

--- Practical Networking For Engineer/Chapter1/Usecase1(Copy).py
+++ Practical Networking For Engineer/Chapter1/Usecase1(Copy).py
@@ -7,108 +7,112 @@ import re # for regex
 
 # Encrypting the set of credentials
 def encryptCredential(pwd):
-    rvalue=base64.b64encode(pwd.encode()) # Encode pwd in the unicode type to UTF_8 and then encode it with base 64
+    rvalue = base64.b64encode(pwd.encode()) # Encode pwd in the unicode type to UTF_8 and then encode it with base 64
     return rvalue
 
 # Decrypting the set of credentials
 def decryptCredential(pwd):
-    rvalue=base64.b64decode(pwd) # Decode base 64
-    rvalue=rvalue.decode() # Decode the unicode type that is the original string.
+    rvalue = base64.b64decode(pwd) # Decode base 64
+    rvalue = rvalue.decode() # Decode the unicode type that is the original string.
     return rvalue
 
 ### procedure for validated customer1
 def validatedCustomer(customer):
     # Some words to say hello and prompt user to issue some information about traveling, time and stuff
     print("Hello "+ customer)
-    inputcity=input("Which country do you want to travel (ex London/Paris/Chicago): ")
-    inputaddinfo=input("Any specific checkin time [AM/PM] and prefered mode of travel [car/bus]: ")
+    inputcity = input("Which country do you want to travel (ex London/Paris/Chicago): ")
+    inputaddinfo = input("Any specific checkin time [AM/PM] and prefered mode of travel [car/bus]: ")
 
-    regex = re.compile('\d+:\d+\s[AP]M')
+    regex = re.compile(r'\d+:\d+\s[AP]M')
     try:
-        time=re.findall(regex, inputaddinfo)
+        time = re.findall(regex, inputaddinfo)
     except Exception as e:
         raise
 
     if "car" in inputaddinfo:
-        transport="car"
+        transport = "car"
     elif "bus" in inputaddinfo:
-        transport="bus"
+        transport = "bus"
     else:
-        transport="Others"
+        transport = "Others"
 
     ### create sentence based upon the additional info provided
     print ("\n\nYou have selected to checkin at "+time[0]+", and your preferred transport will be "+transport+" .")
-    getcityinfo=validatecity(inputcity)
+    getcityinfo = validatecity(inputcity)
 
     ### this is to sort the dictionary from highest to lowest based upon weather types
-    sorted_d = [(k, getcityinfo.get(k)) for k in sorted(getcityinfo, key=getcityinfo.get, reverse=True)]
+    sorted_d  =  [(k, getcityinfo.get(k)) for k in sorted(getcityinfo, key = getcityinfo.get, reverse = True)]
 
     ###iterate through the weathers to construct a sentence
-    sentence= "Weather prediction for next 5 days is (chance of) "
+    sentence =  "Weather prediction for next 5 days is (chance of) "
     for item in sorted_d:
-        sentence=sentence+" "+item[0]+": "+str(item[1])+"%,"
+        sentence = sentence+" "+item[0]+": "+str(item[1])+"%,"
     print(sentence)
 
 
 ### to validate the average weather for that city for next 5 days
 def validatecity(inputcity):
     # Create empty list
-    weathers=[]
-    weatherpercentage={}
+    weathers = []
+    weatherPercentage = {}
 
     # remove any additional spaces accidentally entered
-    inputcity=inputcity.strip()
+    inputcity = inputcity.strip()
 
-    urlx="https://api.openweathermap.org/data/2.5/forecast?q="+inputcity+"&appid=f32a7d9f5cd2ad2d05ff48be965fb0c6"
+    urlx = "https://api.openweathermap.org/data/2.5/forecast?q = "+inputcity+"&appid = f32a7d9f5cd2ad2d05ff48be965fb0c6"
 
     #send the request url using GET method.
-    r = requests.get(url=urlx)
+    r  =  requests.get(url = urlx)
     # convert the preceding data into json
-    output=r.json()
+    output = r.json()
 
     ### this is to parse the type of weather and count them in a list
     for item in output["list"]:
         weathers.append(item["weather"][0]["description"])
-    countweather=Counter(weathers)
+    countWeather = Counter(weathers)
 
     ### this is to find the percentage of the weather from the given output
     for item in countWeather:
-        weatherpercentage[item]=int((countweather[item]/40) * 100)
+        weatherPercentage[item] = int((countWeather[item]/40) * 100)
 
-    return weatherpercentage
+    return weatherPercentage
 
 
 #ask for user name
-uname=input("Enter your username: ")
+try:
+    uname = str(input("Enter your username: "))
+except Exception as e:
+    raise
+
 #ask for password
 #try in cmd or invoke using python command
-p=getpass.getpass(prompt="Enter your password: ")
+p = getpass.getpass(prompt = "Enter your password: ")
 
 #construct credential with *.* as separator between uname and password
-creds=uname+"*.*"+p
+creds = uname+"*.*"+p
 
 
 ##encrypted creds of the registered customers
 #for testing username:password is customer1:password1 ,
 # customer2:password2, and so on
 
-customers = {
+customers  =  {
     "customer1":b'Y3VzdG9tZXIxKi4qcGFzc3dvcmQx',
     "customer2":b'Y3VzdG9tZXIyKi4qcGFzc3dvcmQy',
     "customer3":b'Y3VzdG9tZXIzKi4qcGFzc3dvcmQz'
 }
 
 #to validate whether the user is legitimate
-flag=True
+flag = True
 ### validate if the username is part of any customers
 if (uname in customers):
-    encryptedCreds=encryptCredential(creds)
-    customerCreds=customers[uname]
+    encryptedCreds = encryptCredential(creds)
+    customerCreds = customers[uname]
     ### validate if the credentials provided is the same as stored credentials for that customer
-    if not(str(encryptedCreds.decode()) == str(customerCreds.decode())):
-            flag=False
+    if not(str(encryptedCreds.decode())  ==  str(customerCreds.decode())):
+            flag = False
 else:
-    flag=False
+    flag = False
 
 if not(flag):
     print ("Unauthorized customer.")
--- Practical Networking For Engineer/Chapter1/ValidateIPAdress.py
+++ Practical Networking For Engineer/Chapter1/ValidateIPAdress.py
@@ -1,43 +1,42 @@
-### Validating an IPv4 address
-#=================================Method 1
+import socket
+
+
+# Validating an IPv4 address
+# Method 1
 def method_1():
-    ip_address=input("Enter IP adress: ")
-    #remove any extra characters
-    ip_address=ip_address.strip()
+    ip_address = input("Enter IP adress: ")
 
+    # Remove any extra characters
+    ip_address = ip_address.strip()
 
-    #Initialize a flag to point to true for an ip add
-    ip_address_flag=True
+    # Initialize a flag to point to true for an ip add
+    ip_address_flag = True
 
-    #validate if there are only dots (.) in ip add
+    # validate if there are only dots (.) in ip add
     if (not(ip_address.count('.') == 3)):
-        ip_address_flag=False
-
+        ip_address_flag = False
     else:
-        #Validate if each of the octet is in range 0 - 255
-        ip_address=ip_address.split('.')
+        # Validate if each of the octet is in range 0 - 255
+        ip_address = ip_address.split('.')
         for val in ip_address:
-            val=int(val)
-            if(not(0<= val <= 255)):
-                ip_address_flag=False
+            val = int(val)
+            if(not(0 <= val <= 255)):
+                ip_address_flag = False
 
-    #based upon the flag value display the relevant message
+    # based upon the flag value display the relevant message
     if (ip_address_flag):
         print("Given IP is correct")
     else:
         print("Given IP is not correct")
 
 
-#================================Method 2
-import socket
-
+# ==========================Method 2
 def method_2():
-    addr=input("Enter IP address: ")
+    addr = input("Enter IP address: ")
     try:
-        #give to socket the ip address, and have socket checked the ip add
+        # Give to socket the ip address, and have socket checked the ip add
         socket.inet_aton(addr)
         print("IP add is valid")
-        
-    except socket.error: # if any errors socket finds
-        print("IP add is NOT valid")
 
+    except socket.error:  # if any errors socket finds
+        print("IP add is NOT valid")
